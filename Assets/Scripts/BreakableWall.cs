80920773c12c62f1d8f9c62d9a8dcb4ef5f7bf0c
EstebanNunez <esteban78570@gmail.com>
2024-01-10 11:38:18 +0100

    Fix Breakable Wall and add it to tileset
    - when break the collider was still active

--- Assets/Scripts/BreakableWall.cs
+++ Assets/Scripts/BreakableWall.cs
@@ -8,14 +8,16 @@ public class BreakableWall : MonoBehaviour
     public int life = 5;
     public GameObject wallGo = null;
     public GameObject destroyedWallGo = null;
+    public GameObject colliderGo = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (wallGo == null || destroyedWallGo == null)
+        if (wallGo == null || destroyedWallGo == null || colliderGo == null)
             return;
 
         wallGo.SetActive(true);
+        colliderGo.SetActive(true);
         destroyedWallGo.SetActive(false);
     }
 
@@ -30,10 +32,11 @@ public class BreakableWall : MonoBehaviour
         life -= (attack != null ? attack.damages : 1);
         if (life <= 0)
         {
-            if (wallGo == null || destroyedWallGo == null)
+            if (wallGo == null || destroyedWallGo == null || colliderGo == null)
                 return;
 
             destroyedWallGo.SetActive(true);
+            colliderGo.SetActive(false);
             wallGo.SetActive(false);
         }
     }
@@ -42,7 +45,6 @@ public class BreakableWall : MonoBehaviour
     {
         if ((hitLayers & (1 << collision.gameObject.layer)) == (1 << collision.gameObject.layer))
         {
-            Debug.Log("Hit");
             // Collided with hitbox
             Attack attack = collision.gameObject.GetComponent<Attack>();
             ApplyHit(attack);
