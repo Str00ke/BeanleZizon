801e489f03ac54afccaa5a35cea12d7b23e27b5c
EstebanNunez <esteban78570@gmail.com>
2024-01-17 11:08:58 +0100

    Remove other ElementCollectible on Collect

--- Assets/Scripts/Collectibles/ElementCollectible.cs
+++ Assets/Scripts/Collectibles/ElementCollectible.cs
@@ -1,9 +1,27 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class ElementCollectible : ACollectible
 {
     public Element element = Element.None;
+    public ElementCollectible[] allEllementCollectible;
+
+    private void Start()
+    {
+        allEllementCollectible = FindObjectsOfType<ElementCollectible>();
+    }
 
     protected override void OnCollect()
     {
         Player.Instance.element = element;
+
+        for (int i = allEllementCollectible.Length - 1; i >= 0; --i)
+        {
+            if (allEllementCollectible[i] == this)
+                continue;
+
+            Destroy(allEllementCollectible[i].gameObject);
+        }
     }
 }
